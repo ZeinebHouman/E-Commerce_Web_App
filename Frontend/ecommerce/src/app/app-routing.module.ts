deb23ef7d12eb0267ea41973b31234bd9b089ce3
Mohamed Ben Chamakh <mohamed.ben.chamakh98@gmail.com>
2021-03-13 21:11:25 +0100

    Page accueil,connexion,inscription

--- Frontend/ecommerce/src/app/app-routing.module.ts
+++ Frontend/ecommerce/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+
+import { AccueilComponent } from './component/accueil/accueil.component';
+import { SignInComponent } from './component/sign-in/sign-in.component';
+import { SignUpComponent } from './component/sign-up/sign-up.component';
+
+const routes: Routes = [
+  {path:"",component:AccueilComponent},
+  {path:"SignIn",component:SignInComponent},
+  {path:"SignUp",component:SignUpComponent},
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/ecommerce/src/app/app.module.ts
+++ Frontend/ecommerce/src/app/app.module.ts
@@ -4,9 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './component/template/header/header.component';
+import { AccueilComponent } from './component/accueil/accueil.component';
+import { MenuComponent } from './component/template/menu/menu.component';
+import { FooterComponent } from './component/template/footer/footer.component';
+import { SignInComponent } from './component/sign-in/sign-in.component';
+import { SignUpComponent } from './component/sign-up/sign-up.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AccueilComponent,
+    MenuComponent,
+    FooterComponent,
+    SignInComponent,
+    SignUpComponent
+
   ],
   imports: [
     BrowserModule,
